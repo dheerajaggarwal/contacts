2b37f4ea2537ad1b9bb905b46ca4171b7276f72d
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2019-11-27 15:58:13 +0530

    minor update

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
 var express = require('express'),
-    path = require('path'),
-    http = require('http'),
-    contact = require('./routes/contacts'),
-    home = require('./routes/home');
+  path = require('path'),
+  http = require('http'),
+  contact = require('./routes/contacts'),
+  home = require('./routes/home');
 
 var app = express(),
-    baseURL = process.env.baseURL || '/',
-    instanceType = 'Test';
+  baseURL = process.env.baseURL || '/',
+  instanceType = 'Test';
 
-if(baseURL.indexOf('prod') !== -1){
-    instanceType = 'Production';
+if (baseURL.indexOf('prod') !== -1) {
+  instanceType = 'Production';
 }
 
 app.locals = {
-    baseURL: process.env.baseURL || '/',
-    instanceType: instanceType
+  baseURL: process.env.baseURL || '/',
+  instanceType: instanceType
 };
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-    app.set('views', path.join(__dirname, 'public'));
-    app.engine('html', require('ejs').renderFile);
-    app.set('view engine', 'html');
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+  app.use(express.bodyParser());
+  app.set('views', path.join(__dirname, 'public'));
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-var requiresLogin = function(req, res, next) {
-    next();
-    /*var redirectURL = '/signin';
-    if (!req.isAuthenticated()) {
-    	return res.redirect(redirectURL);
-    } else next();*/
+var requiresLogin = function (req, res, next) {
+  next();
 };
 
 app.post('/contacts/rePopulateDB', requiresLogin, contact.rePopulateDB);
@@ -46,5 +42,5 @@ app.delete('/contacts/:id', requiresLogin, contact.deletecontact);
 app.get('/', home.index);
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
